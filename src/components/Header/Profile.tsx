91bd6f6fcec587aad423766a6e9bf486b7a96f23
juan.monteirov@gmail.com <juan.monteirov@gmail.com>
2022-02-06 13:26:59 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -4,15 +4,20 @@ const username = "Juan Veronez";
 const email = "juan.monteirov@gmail.com";
 const userAvatar = "https://github.com/juanmveronez.png"
 
-export function Profile() {
+type ProfileProps = {
+  showFullProfileData: boolean
+}
+
+export function Profile({ showFullProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>{username}</Text>
-        <Text color="gray.300" fontSize="small"
-        >{email}</Text>
-      </Box>
-      
+      {showFullProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>{username}</Text>
+          <Text color="gray.300" fontSize="small"
+          >{email}</Text>
+        </Box>
+      )}
       {!!userAvatar 
       ? <Avatar size="md" name={username} src={userAvatar}/>
       : <Avatar size="md" name={username}/>
