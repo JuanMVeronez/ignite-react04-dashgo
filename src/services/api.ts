5f6f41820301c565e074ae391f2b113e60787377
Juan Veronez <juan.monteiro@venturus.org.br>
2022-03-15 07:54:51 -0300

    auth and ssr auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,69 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from 'nookies';
 
-const fakeApiUrl = "http://localhost:3000/api"
+const apiUrl = "http://localhost:3000/api";
+
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
 
 export const api = axios.create({
-  baseURL: fakeApiUrl
+  baseURL: apiUrl,
+  headers: {
+    Authorization: cookies['dashgo.token'],
+  },
+});
+
+api.interceptors.response.use(response => response, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+      const { 'dashgo.refreshToken': refreshToken } = cookies;
+      const originalConfig = error.config;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const { token, refreshToken } = response.data;
+  
+          setCookie(undefined, 'dashgo.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/',
+          });
+
+          setCookie(undefined, 'dashgo.refreshToken', refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/',
+          });
+
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+        
+          failedRequestsQueue.forEach(request => request.onSuccess(token))
+          failedRequestsQueue = [];
+        }).catch(err => {
+          failedRequestsQueue.forEach(request => request.onFailure(err));
+          failedRequestsQueue = [];
+        }).finally(() => {
+          isRefreshing = false;
+        })
+      }
+      
+      return new Promise((resolve, refect) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+            resolve(api(originalConfig));
+          },
+          onFailure: (err: AxiosError) => {
+            refect(err);
+          },
+        })
+      })
+    } else {
+      // logout
+    }
+  }
 })
